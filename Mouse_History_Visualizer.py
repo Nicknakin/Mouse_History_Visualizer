44c7b5d5eca22a729a91538b64bbd58691807517
Sourceduty <123030236+sourceduty@users.noreply.github.com>
2023-10-26 01:11:11 -0400

    Add files via upload

---
+++ Mouse_History_Visualizer.py
@@ -0,0 +1,49 @@
+# Moust_History_Visualizer
+
+# 🖱️ Visualize mouse pointer movement history.
+
+# Copyright (C) 2023, Sourceduty - All Rights Reserved.
+# THE CONTENTS OF THIS PROJECT ARE PROPRIETARY.
+
+import pygame
+import pyautogui
+import sys
+
+# Initialize Pygame
+pygame.init()
+
+# Constants
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+BACKGROUND_COLOR = (0, 0, 0)
+LINE_COLOR = (255, 255, 255)
+LINE_WIDTH = 2
+MOUSE_HISTORY = []
+
+# Create the screen
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Mouse_History_Visualizer")
+
+# Main loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Get the current mouse position
+    mouse_x, mouse_y = pyautogui.position()
+    MOUSE_HISTORY.append((mouse_x, mouse_y))
+
+    # Clear the screen
+    screen.fill(BACKGROUND_COLOR)
+
+    # Draw the mouse pointer history
+    if len(MOUSE_HISTORY) > 1:
+        pygame.draw.lines(screen, LINE_COLOR, False, MOUSE_HISTORY, LINE_WIDTH)
+
+    pygame.display.flip()
+
+# Quit Pygame
+pygame.quit()
+sys.exit()
